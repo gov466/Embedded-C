7fa20b6227c43cfe4ad13203ce36440fe65fbf04
Govind raj <govind.mndy@gmail.com>
2019-10-24 22:28:43 -0400

    Create ascending.c

---
+++ C program/ascending.c
@@ -0,0 +1,32 @@
+// Example program to sort array inascending order
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int a[5],i,temp,j;
+    cout<<"enter elements";
+    for(i=0;i<5;i++)
+    {
+        cin>>a[i];
+    }
+     for(i=0;i<5;i++)
+     {
+         for(j=i+1;j<5;j++)
+         {
+             
+             if(a[j]<a[i])
+              {
+             temp=a[i];
+             a[i]=a[j];
+             a[j]=temp;
+              }
+         }
+     }
+    cout<<"new array";
+    for(i=0;i<5;i++)
+    {
+        cout<<a[i];
+    }
+    
+}

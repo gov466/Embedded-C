f5eee2090ddf3319bbd6cf0128b5dc01b622879e
Govind raj <govind.mndy@gmail.com>
2019-11-12 23:02:19 -0500

    Update Frwd_difference.cpp

--- C++/Frwd_difference.cpp
+++ C++/Frwd_difference.cpp
@@ -1,3 +1,7 @@
+/*Write an application to calculate the value of f(x)=sin(x) at x=45 degrees 
+based on the Maclaurin series. In order to calculate the derivatives of this function
+use “Forward Difference” method with step size of 1 degree.   */
+
 #include <iostream>
 #include <cmath>
 using namespace std;

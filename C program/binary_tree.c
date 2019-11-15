4fd0c0ad6642ddcd2c78720abfc77d19338f3c34
Govind raj <govind.mndy@gmail.com>
2019-11-14 23:54:32 -0500

    Create binary_tree.c

---
+++ C program/binary_tree.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct BstNode {
+    int data;
+    struct BstNode* left;
+    struct BstNode* right;
+};
+typedef struct BstNode node;
+node* GetNewNode(int data)
+    {
+        node* newNode = (node*)malloc(sizeof(node));
+        newNode->data = data;
+        newNode->left = newNode->right = NULL;
+        return newNode;
+
+}
+node* Insert(node* rootPtr, int data)
+{
+    if(rootPtr == NULL)
+    {
+    rootPtr = GetNewNode(data);
+    }
+    else if(data <= rootPtr->data)
+    {
+    rootPtr->left = Insert(rootPtr->left, data);
+    }
+    else
+    {
+    rootPtr->right = Insert(rootPtr->right, data);
+    }
+    return rootPtr;
+}
+int Search(struct BstNode* rootPtr, int data)
+{
+    if(rootPtr == NULL) 
+    {
+    return 0;
+    }
+    else if(rootPtr->data == data)
+    {
+    return 1;
+    }
+    else if(data <= rootPtr->data) 
+    {
+    return Search(rootPtr->left, data);
+    }
+    else {
+    return Search(rootPtr->right, data);
+    }
+}
+int main(void){
+node* rootPtr;
+rootPtr = NULL;
+rootPtr = Insert(rootPtr, 15);
+rootPtr = Insert(rootPtr, 10);
+rootPtr = Insert(rootPtr, 20);
+rootPtr = Insert(rootPtr, 05);
+rootPtr = Insert(rootPtr, 30);
+rootPtr = Insert(rootPtr, 25);
+int n;
+printf("Enter number to be searched: \n");
+scanf("%d",&n);
+if(Search(rootPtr, n) == 1) {
+printf("Found\n");
+}
+else {
+printf("Not Found\n");
+}
+return 0;
+}
